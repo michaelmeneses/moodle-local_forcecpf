a7f9fa7e3f7723cc1fddb841bb0623660e98f4c8
Michael Meneses <michael.douglas.meneses.2@gmail.com>
2017-07-21 15:13:27 -0300

    FIX lang errormessagedesc

--- lang/pt_br/local_forcecpf.php
+++ lang/pt_br/local_forcecpf.php
@@ -14,6 +14,7 @@ $string['cpf'] = 'CPF';
 $string['custommessage'] = 'Mensagem personalizada';
 $string['custommessagedesc'] = 'Personalize a mensagem apresentada na página de atualização do cadastro.';
 $string['errormessage'] = '<p>Prezado(a) estudante,</p><p>Não foi possível atualizar seus dados.</p><p>Por favor, entre em contato com o suporte.</p>';
+$string['errormessagedesc'] = 'Personalize a mensagem apresentada em caso de erro no processamento do formulário.';
 $string['invalidcpf'] = 'CPF Inválido';
 $string['message'] = '<p>Prezado(a) estudante,</p><p>Seu login de acesso a plataforma será seu CPF.</p><p>Informe seu CPF no campo abaixo para atualização do seu cadastro.</p>';
 $string['pluginname'] = 'Força CPF';
